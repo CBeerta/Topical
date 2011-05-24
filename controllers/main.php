a1faa6862b43ff1616b115f71a3d8c895bf6fe45
Claus Beerta <claus@beerta.de>
2011-05-24 14:32:37 +0200

    Revert "Revert "Added jQuery UI. Task list is now Sortable.""
    This reverts commit a35142d452b4f58778ee4f4d862adad88d3a04b1.

--- controllers/main.php
+++ controllers/main.php
@@ -3,42 +3,11 @@
 function main_index( $day )
 {
     $day = $day ? $day : date('Y-m-d');
-    
-    set('yesterday', 'Yesterday');
-    set('tomorrow', 'Tomorrow');
+    set('day', $day);
 
-    set ('title', 'Today');
-    set ('day', $day);
-    
-    set('hours', range(option('daystart_hour'), option('dayend_hour'), 1));
-    
-    $todo = ORM::for_table('todo')
-        ->where_raw("`completed` IS NULL")
-        ->find_many();
-    set('todo', $todo);
-    
-    $completed = ORM::for_table('todo')
-        ->select_expr("STRFTIME('%H', `completed`, 'localtime')", 'hour')
-        ->select('*')
-        ->where_raw("DATE(`completed`) = ?", array($day))
-        ->find_many();
+    set('todolist', _todo_index($day));
+    set('calendar', _calendar_index($day));
 
-    $indexed_completed = array();
-    foreach ($completed as $item)
-    {
-        $indexed_completed[$item->hour][] = (object) array(
-            'id' => $item->id,
-            'content' => $item->content,
-            'hour' => $item->hour,
-            );
-    }
-        
-    set('completed', $indexed_completed);
-    
-
-    set('todolist', partial('todolist.html.php'));
-    set('calendar', partial('calendar.html.php'));
-    
     return html('main.html.php');
 }
 
@@ -59,3 +28,7 @@ function main_post( $day )
 
     return redirect('/' . $day);
 }
+
+
+
+
